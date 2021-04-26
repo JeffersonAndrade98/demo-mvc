9465ffa81b7ec622d81bffa4c61acc7725cb9d0f
JEFF_ANDRADE <hewlett.stark@gmail.com>
2021-04-26 08:29:50 -0300

    Manipulando pagina de erro, modulo 13 .3

--- src/main/java/com/mballem/curso/boot/web/error/MyErrorView.java
+++ src/main/java/com/mballem/curso/boot/web/error/MyErrorView.java
@@ -20,7 +20,7 @@ public class MyErrorView implements ErrorViewResolver {
 		switch (status.value()) {
 		
 		case 400: 
-			model.addObject("error", "Erro inseperado");
+			model.addObject("error", "Erro inesperado");
 			model.addObject("message", "O parâmetro de solicitação necessário {tal} para o tipo de parâmetro do método {tal} está presente, mas foi convertido em nulo");
 			break;
 		
